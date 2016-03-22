3a8c50eb87954d863830096a9eae14dc7dae0ee2
John James Jacoby <johnjamesjacoby@me.com>
2016-03-22 14:58:19 -0500

    Improve targeting for admin pages.
    No Help on term.php.

--- class-wp-term-meta-ui.php
+++ class-wp-term-meta-ui.php
@@ -158,9 +158,15 @@ class WP_Term_Meta_UI {
 
 		// Only blog admin screens
 		if ( is_blog_admin() || doing_action( 'wp_ajax_inline_save_tax' ) ) {
-			add_action( 'admin_init',         array( $this, 'admin_init'     ) );
-			add_action( 'load-edit-tags.php', array( $this, 'edit_tags_page' ) );
-			add_action( 'load-term.php',      array( $this, 'term_page'      ) );
+
+			// Every admin page
+			add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+			// Bail if taxonomy does not include colors
+			if ( ! empty( $_REQUEST['taxonomy'] ) && in_array( $_REQUEST['taxonomy'], $this->taxonomies, true ) ) {
+				add_action( 'load-edit-tags.php', array( $this, 'edit_tags_page' ) );
+				add_action( 'load-term.php',      array( $this, 'term_page'      ) );
+			}
 		}
 	}
 
@@ -231,19 +237,10 @@ class WP_Term_Meta_UI {
 	 * @since 0.1.0
 	 */
 	public function edit_tags_page() {
-
-		// Bail if taxonomy does not include colors
-		if ( empty( $_REQUEST['taxonomy'] ) || ! in_array( $_REQUEST['taxonomy'], $this->taxonomies, true ) ) {
-			return;
-		}
-
-		// Enqueue javascript
 		add_action( 'admin_head-edit-tags.php',          array( $this, 'help_tabs'       ) );
 		add_action( 'admin_head-edit-tags.php',          array( $this, 'admin_head'      ) );
 		add_action( 'admin_print_scripts-edit-tags.php', array( $this, 'enqueue_scripts' ) );
-
-		// Quick edit
-		add_action( 'quick_edit_custom_box', array( $this, 'quick_edit_meta' ), 10, 3 );
+		add_action( 'quick_edit_custom_box',             array( $this, 'quick_edit_meta' ), 10, 3 );
 	}
 
 	/**
@@ -252,14 +249,6 @@ class WP_Term_Meta_UI {
 	 * @since 0.1.9
 	 */
 	public function term_page() {
-
-		// Bail if taxonomy does not include colors
-		if ( empty( $_REQUEST['taxonomy'] ) || ! in_array( $_REQUEST['taxonomy'], $this->taxonomies, true ) ) {
-			return;
-		}
-
-		// Enqueue javascript
-		add_action( 'admin_head-term.php',          array( $this, 'help_tabs'       ) );
 		add_action( 'admin_head-term.php',          array( $this, 'admin_head'      ) );
 		add_action( 'admin_print_scripts-term.php', array( $this, 'enqueue_scripts' ) );
 	}
